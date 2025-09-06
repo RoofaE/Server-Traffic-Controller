651f814a3a88053c92b17e18130deb04699be1a9
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2025-09-05 19:21:28 -0600

    added start and stop methods for traffic generation

--- src/traffic_generator.py
+++ src/traffic_generator.py
@@ -19,4 +19,36 @@ class TrafficGenerator:
             load_balancer: LoadBalancer instance to send requests to
             pattern: Which kind of traffic pattern to simulate
         """
-        
+        self.load_balancer = load_balancer
+        self.pattern = pattern
+        self.request_counter = 0
+        self.is_running = False
+        self.generator_thread = None
+
+        print(f"Traffic Generator createdwith {pattern.value} pattern")
+    
+    def start(self):
+        """
+        Start generating the traffic
+        """
+        if self.is_running:
+            print("Traffic Generator is running")
+            return
+
+        self.is_running = True
+        self.generator_thread = threading.Thread(target=self._generate_traffic)
+        self.generator_thread.start()
+
+        print("Traffic Generation has started")
+
+    
+    def stop(self):
+        """
+        Stop generating the traffic
+        """
+        self.is_running = False
+        if self.generator_thread:
+            self.generator_thread.join()
+        print("Traffic Generation stopped")
+    
+
