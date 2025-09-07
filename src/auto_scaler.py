feb7dd9647a93731e5ed5ac198b59732bff85d09
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2025-09-07 17:14:13 -0600

    added monitoring servers scalings based on business

--- src/auto_scaler.py
+++ src/auto_scaler.py
@@ -27,4 +27,43 @@ class AutoScaler:
 
     
     def start(self):
+        """
+        Start the auto scaler monitoring
+        """
+
+        self.is_running = True
+        threading.Thread(target=self._monitor).start()
+        print("Auto Scaler started")
+    
+    def stop(self):
+        """
+        Stop the auto scaler
+        """
+        self.is_running = False
+        print("Auto Scaler stopped")
+    
+    def _monitor(self):
+        """
+        Check system load and scale up or down as needed
+        """
+        while self.is_running:
+            time.sleep(5) # check every 5 seconds
+
+            # not too often
+            if time.time() - self.last_scale_time < 10:
+                continue
+
+            stats = self.load_balancer.get_stats()
+            util = stats['util']
+            server_count = stats['total_servers']
+
+            # scale up if busy
+            if util > 70 and server_count < self.max_servers:
+                self._add_server()
+            
+            # scale down if quiet
+            elif util < 30 and server_count > self.min_servers:
+                self._remove_server()
+    
+    def _add_server(self):
         pass
