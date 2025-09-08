adcc87f944c00f2a2cdf2fc70eafa2ec59d2c4be
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2025-09-08 15:55:13 -0600

    added system setup method

--- main.py
+++ main.py
@@ -24,8 +24,39 @@ def welcome():
     print("-> Real time auto scaling based on the metrics")
     print("-> Concurrent request processing")
     print("-> Live dashboard")
-    
+
     print("=============================================================================================")
 
+def system_setup():
+    """
+    Initializes all system components
+    """
+
+    print("\n Setting up system components")
+
+    lb = LoadBalancer(RoutingAlgo.ROTATING)
+
+    print("Adding servers:")
+    initial_servers = [
+        Server("primary-1", max_capacity=3, base_response_time=0.4),
+        Server("primary-2", max_capacity=4, base_response_time=0.5)
+    ]
+
+    for server in initial_servers:
+        lb.add_server(server)
+    
+    # create traffic for burst pattern
+    traffic_gen = TrafficGenerator(lb, TrafficPattern.BURST)
+
+    # auto scaler creator
+    auto_scaler = AutoScaler(lb, min_servers=2, max_servers=8)
+
+    # make dashboard
+    dashboard = Dashboard(lb, traffic_gen, auto_scaler)
+    print("System setup is complete")
+
+    return lb, traffic_gen, auto_scaler, dashboard
+
+    
 
 
